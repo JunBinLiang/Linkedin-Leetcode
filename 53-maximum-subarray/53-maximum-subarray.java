ca933612bfc62c71fbd6c9a72de19196a9805a2e
66brother <45537132+JunBinLiang@users.noreply.github.com>
2022-06-17 19:12:41 -0400

    Time: 1 ms (100.00%), Space: 51.4 MB (96.45%) - LeetHub

---
+++ 53-maximum-subarray/53-maximum-subarray.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int maxSubArray(int[] a) {
+        int res = -1000000000;
+        int sum = 0, mn = 0;
+        for(int i = 0; i < a.length; i++) {
+            sum += a[i];
+            res = Math.max(res, sum - mn);
+            mn = Math.min(mn, sum);
+        }
+        return res;
+    }
+}
