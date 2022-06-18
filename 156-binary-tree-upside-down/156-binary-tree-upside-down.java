b30ed4feeec36846afd6aaee6303043ac0114c96
66brother <45537132+JunBinLiang@users.noreply.github.com>
2022-06-18 00:08:44 -0400

    Time: 1 ms (30.10%), Space: 41.7 MB (65.08%) - LeetHub

---
+++ 156-binary-tree-upside-down/156-binary-tree-upside-down.java
@@ -0,0 +1,46 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+//start : 11 : 56
+
+class Solution {
+    public TreeNode upsideDownBinaryTree(TreeNode root) {
+        if(root == null) {
+            return null;
+        }
+        TreeNode left = root.left;
+        TreeNode right = root.right;
+        root.left = null;
+        root.right = null;
+        return dfs(left, root, right);   
+    }
+    
+    public TreeNode dfs(TreeNode root, TreeNode last, TreeNode right) {
+        if(root != null) {
+            TreeNode lleft = root.left;
+            TreeNode lright = root.right;
+            root.right = last;
+            root.left = right;
+            return dfs(lleft, root, lright);
+            
+        } else {
+            return last;
+        }
+        
+ 
+
+    }
+}
