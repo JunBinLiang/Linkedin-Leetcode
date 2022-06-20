7902297ad6e9a4416018d06fc6efb8ea1da2e6fa
66brother <45537132+JunBinLiang@users.noreply.github.com>
2022-06-19 22:03:45 -0400

    Time: 1 ms (99.70%), Space: 44.4 MB (95.66%) - LeetHub

---
+++ 57-insert-interval/57-insert-interval.java
@@ -0,0 +1,66 @@
+class Solution {
+    public int[][] insert(int[][] a, int[] b) {
+        if(a.length == 0) {
+            int res[][] = new int[1][2];
+            res[0] = b;
+            return res;
+        };
+        
+        int n = a.length;
+        int s = b[0], e = b[1];
+        
+        boolean first = false;
+        List<int[]> res = new ArrayList<>();
+        
+        for(int i = 0; i < n; i++) {
+            if(s <= a[i][1] && e >= a[i][0]) {
+                first = true;
+                int j = i + 1;
+                int newEnd = Math.max(e, a[i][1]);
+                while(j < n && a[j][0] <= e) {
+                    newEnd = Math.max(newEnd, a[j][1]);
+                    j++;
+                }
+                res.add(new int[]{Math.min(a[i][0], s), newEnd});
+                i = j - 1;
+                
+            } else {
+                res.add(a[i]);
+            }    
+        }
+        
+        
+        if(!first) {
+            res.clear();
+            if(b[1] < a[0][0]) {
+                res.add(b);
+            }
+            
+            
+            for(int i = 0; i < n; i++) {
+                res.add(a[i]);
+                if(i + 1 < n && b[0] > a[i][1] && b[1] < a[i + 1][0]) {
+                    res.add(b);
+                }
+            }
+            
+            if(b[0] > a[n - 1][1]) {
+                res.add(b);
+            }
+            
+            return change(res);
+        }
+        
+        
+        return change(res);
+        
+    }
+    
+    public int[][] change(List<int[]> list) {
+        int res[][] = new int[list.size()][2];
+        for(int i = 0; i < res.length; i++) {
+            res[i] = list.get(i);
+        }
+        return res;
+    }
+}
