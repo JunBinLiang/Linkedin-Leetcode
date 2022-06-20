9b7bc7bce194798d43267bb2e13308eea7466dbf
66brother <45537132+JunBinLiang@users.noreply.github.com>
2022-06-19 22:17:38 -0400

    Time: 14 ms (48.88%), Space: 55.8 MB (12.93%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.java
+++ 56-merge-intervals/56-merge-intervals.java
@@ -1,8 +1,14 @@
 class Solution {
     public int[][] merge(int[][] a) { 
-        Arrays.sort(a, (x, y) -> {
+        Arrays.sort(a, (x, y) -> { //x y 1d
             return x[0] - y[0];
         });
+        
+        //[s1, e1]
+        //[s2, e2]
+        //[s3, e3]
+        //[s4, e4]
+        
         Stack<int[]> sta = new Stack<>();
         for(int p[] : a) {
             if(sta.size() == 0) {
@@ -16,11 +22,12 @@ class Solution {
                 }
             }
         }
-        
         int res[][] = new int[sta.size()][2];
         for(int i = res.length - 1; i >= 0; i--) {
             res[i] = sta.pop();
         }
+        
+        
         return res;
     }
 }
