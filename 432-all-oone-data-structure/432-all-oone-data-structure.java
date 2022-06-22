270b60a8a6685c8b12d5c4273193c368b3a24693
66brother <45537132+JunBinLiang@users.noreply.github.com>
2022-06-21 22:29:12 -0400

    Time: 92 ms (59.43%), Space: 107.7 MB (6.13%) - LeetHub

--- 432-all-oone-data-structure/432-all-oone-data-structure.java
+++ 432-all-oone-data-structure/432-all-oone-data-structure.java
@@ -1,97 +1,123 @@
 class AllOne {
-    List<Set<String>> list = new ArrayList<>();
     Map<String, Integer> f = new HashMap<>();
-    int cnt = 0;
-    
-    int mnIdx = -1, mxIdx = -1;
+    Map<Integer, ListNode> g = new HashMap<>();
+    ListNode head = new ListNode();
+    ListNode tail = new ListNode();
     public AllOne() {
-        list.add(new HashSet<>());
+        head.next = tail;
+        tail.pre = head;
     }
     
     public void inc(String key) {
         if(!f.containsKey(key)) {
-            cnt++;
-            if(list.size() == 1) list.add(new HashSet<>());
             f.put(key, 1);
-            list.get(1).add(key);
-            if(mnIdx == -1 && mxIdx == -1) {
-                mnIdx = 1;
-                mxIdx = 1;
+            if(g.containsKey(1)) {
+                g.get(1).set.add(key);
             } else {
-                mxIdx = Math.max(mxIdx, 1);
-                mnIdx = Math.min(mnIdx, 1);
+                ListNode node = new ListNode();
+                g.put(1, node);
+                node.set.add(key);
+                add(head, node);
             }
-            
             
         } else {
             int oldCnt = f.get(key);
-            if(list.size() == oldCnt + 1) list.add(new HashSet<>());
             f.put(key, oldCnt + 1);
-            
-            list.get(oldCnt).remove(key);
-            list.get(oldCnt + 1).add(key);
-            
-            mxIdx = Math.max(mxIdx, oldCnt + 1);
-            if(mnIdx == oldCnt && list.get(oldCnt).size() == 0) {
-                mnIdx++;
+            if(g.containsKey(oldCnt + 1)) {
+                g.get(oldCnt + 1).set.add(key);
+                g.get(oldCnt).set.remove(key);
+                if(g.get(oldCnt).set.size() == 0) {
+                    delete(g.get(oldCnt));
+                    g.remove(oldCnt);
+                }
+            } else {
+                ListNode node = new ListNode();
+                g.put(oldCnt + 1, node);
+                add(g.get(oldCnt), node);
+                g.get(oldCnt).set.remove(key);
+                node.set.add(key);
+                if(g.get(oldCnt).set.size() == 0) {
+                    delete(g.get(oldCnt));
+                    g.remove(oldCnt);
+                }
             }
-            
         }
-        //System.out.println("inc " + f + " " + mnIdx + " " + mxIdx);
     }
     
     public void dec(String key) {
         int oldCnt = f.get(key);
-        list.get(oldCnt).remove(key);
         if(oldCnt == 1) {
-            cnt--;
             f.remove(key);
-            if(cnt == 0) {
-                mnIdx = -1;
-                mxIdx = -1;
-            } else {
-                for(int i = 1; i < list.size(); i++) {
-                    if(list.get(i).size() > 0) {
-                        mnIdx = i;
-                        break;
-                    }
-                }
+            g.get(1).set.remove(key);
+            if(g.get(1).set.size() == 0) {
+                delete(g.get(1));
+                g.remove(1);
             }
-            
-            
         } else {
             f.put(key, oldCnt - 1);
-            list.get(oldCnt - 1).add(key);
-            if(mxIdx == oldCnt && list.get(oldCnt).size() == 0) {
-                mxIdx--;
+            if(g.containsKey(oldCnt - 1)) {
+                g.get(oldCnt - 1).set.add(key);
+                g.get(oldCnt).set.remove(key);
+                if(g.get(oldCnt).set.size() == 0) {
+                    delete(g.get(oldCnt));
+                    g.remove(oldCnt);
+                }
+            } else {
+                ListNode node = new ListNode();
+                node.set.add(key);
+                g.get(oldCnt).set.remove(key);
+                ListNode pre = g.get(oldCnt).pre;
+                g.put(oldCnt - 1,node);
+                add(pre, node);
+                if(g.get(oldCnt).set.size() == 0) {
+                    delete(g.get(oldCnt));
+                    g.remove(oldCnt);
+                }
             }
-            mnIdx = Math.min(mnIdx, oldCnt - 1);
         }
-        
     }
     
     public String getMaxKey() {
-        if(mxIdx == -1) return "";
+        if(f.size() == 0) return "";
+        ListNode node = tail.pre;
         String res = "";
-        Set<String> set = list.get(mxIdx); 
-        for(String s : set) {
+        for(String s : node.set) {
             res = s;
             break;
         }
-       // System.out.println("max  " + f);
         return res;
     }
     
     public String getMinKey() {
-        if(mnIdx == -1) return "";
+        if(f.size() == 0) return "";
+        ListNode node = head.next;
         String res = "";
-        Set<String> set = list.get(mnIdx); 
-        for(String s : set) {
+        for(String s : node.set) {
             res = s;
             break;
         }
-        //System.out.println("min  " + f+"  " + mnIdx);
         return res;
+    }
+    
+    public void add(ListNode pre, ListNode cur) {
+        ListNode nxt = pre.next;
+        pre.next = cur;
+        cur.pre = pre;
+        cur.next = nxt;
+        nxt.pre = cur;
+    }
+    
+    public void delete(ListNode node) {
+        ListNode pre = node.pre;
+        ListNode nxt = node.next;
+        pre.next = nxt;
+        nxt.pre = pre;
+    }
+    
+    class ListNode {
+        Set<String> set = new HashSet<>();
+        ListNode next;
+        ListNode pre;
     }
 }
 
@@ -103,10 +129,3 @@ class AllOne {
  * String param_3 = obj.getMaxKey();
  * String param_4 = obj.getMinKey();
  */
-
-//[string, counter]
-
-/*
-["AllOne","inc","inc","inc","inc","inc","inc","dec", "dec","getMinKey","dec","getMaxKey","getMinKey"]
-[[],["a"],["b"],["b"],["c"],["c"],["c"],["b"],["b"],[],["a"],[],[]]
-*/
