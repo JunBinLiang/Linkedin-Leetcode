9720ca0df4f01ee9c46cb42c5e07dd48282a8224
66brother <45537132+JunBinLiang@users.noreply.github.com>
2022-07-08 18:52:01 -0400

    Time: 203 ms (47.60%), Space: 95.1 MB (85.40%) - LeetHub

---
+++ 2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph/2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph.java
@@ -0,0 +1,48 @@
+class Solution {
+    public List<List<Integer>> getAncestors(int n, int[][] edges) {
+        List<Integer> graph[] = new ArrayList[n];
+        int in[] = new int[n];
+        Set<Integer> sets[] = new TreeSet[n];
+        
+        /*for(int i = 0; i < n; i++) {
+            graph[i] = new ArrayList<>();
+        }*/
+        Arrays.setAll(graph, e -> new ArrayList<>());
+        Arrays.setAll(sets, e -> new TreeSet<>());
+        
+        for(int edge[] : edges) {
+            //edge[0] -> edge[1]  u -> v
+            int u = edge[0], v = edge[1];
+            graph[u].add(v);
+            in[v] += 1;
+        }
+        
+        Queue<Integer> q = new LinkedList<>();
+        for(int i = 0; i < n; i++) {
+            if(in[i] == 0) {
+                //no pre, can take
+                q.add(i);
+            }
+        } 
+        
+        while(q.size() > 0) {
+            int root = q.poll();
+            for(int nxt : graph[root]) {
+                sets[nxt].add(root);
+                sets[nxt].addAll(sets[root]);
+                in[nxt]--;
+                if(in[nxt] == 0) {
+                    q.add(nxt);
+                }
+            }
+        }
+        
+       
+        List<List<Integer>> res = new ArrayList<>();
+        for(int i = 0; i < n; i++) res.add(new ArrayList<>(sets[i]));
+        return res;
+        
+    }
+}
+
+//sets[i] : all ancestor of i
