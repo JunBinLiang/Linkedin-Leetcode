4f66d8956b39d7632930610267d9569d4535b9ea
66brother <45537132+JunBinLiang@users.noreply.github.com>
2022-07-10 20:58:18 -0400

    Time: 15 ms (7.90%), Space: 42.1 MB (85.93%) - LeetHub

--- 256-paint-house/256-paint-house.java
+++ 256-paint-house/256-paint-house.java
@@ -5,13 +5,25 @@ class Solution {
         for(int i = 1; i < a.length; i++) {
             for(int j = 0; j < 3; j++) { //pain j 
                 int min = Integer.MAX_VALUE;
-                for(int k : last[j]) {
+                /*for(int k : last[j]) {
                     min = Math.min(min, a[i - 1][k]);
+                }*/
+                
+                for(int last = 0; last < 3; last++) {
+                    if(last == j) continue;
+                    min = Math.min(min, a[i - 1][last]);
                 }
+                
                 a[i][j] += min;
             }
         }
         
+        for(int p[] : a) System.out.println(Arrays.toString(p));
+        
         return Math.min(a[n - 1][0], Math.min(a[n - 1][1], a[n - 1][2]));
     }
 }
+
+//[[17,2,17]      [17 2 17]
+//[16,16,5]       [18, 33, 7]
+//[14,3,19]]      [21, 10, 37]
