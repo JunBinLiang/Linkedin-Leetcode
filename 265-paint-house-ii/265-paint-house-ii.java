489fc5f305abf66a27b048d0498fabd6796c3b15
66brother <45537132+JunBinLiang@users.noreply.github.com>
2022-07-10 21:12:40 -0400

    Time: 3 ms (88.45%), Space: 42.3 MB (92.20%) - LeetHub

--- 265-paint-house-ii/265-paint-house-ii.java
+++ 265-paint-house-ii/265-paint-house-ii.java
@@ -1,6 +1,51 @@
 class Solution {
     public int minCostII(int[][] a) {
         int k = a[0].length;
+        int l[] = new int[k];
+        int r[] = new int[k];
+        
+        for(int j = 0; j < k; j++) {
+            l[j] = a[0][j];
+            if(j - 1 >= 0) l[j] = Math.min(l[j], l[j - 1]);
+        }
+        
+        for(int j = k - 1; j >= 0; j--) {
+            r[j] = a[0][j];
+            if(j + 1 < k) r[j] = Math.min(r[j], r[j + 1]);
+        }
+        
+        
+        for(int i = 1; i < a.length; i++) {
+            for(int j = 0; j < k; j++) {
+                int min = Integer.MAX_VALUE;
+                if(j - 1 >= 0) min = Math.min(min, l[j - 1]);
+                if(j + 1 < k) min = Math.min(min, r[j + 1]);
+                a[i][j] += min; 
+            }
+            
+            for(int j = 0; j < k; j++) {
+                l[j] = a[i][j];
+                if(j - 1 >= 0) l[j] = Math.min(l[j], l[j - 1]);
+            }
+
+            for(int j = k - 1; j >= 0; j--) {
+                r[j] = a[i][j];
+                if(j + 1 < k) r[j] = Math.min(r[j], r[j + 1]);
+            }
+        }
+        
+        int res = Integer.MAX_VALUE;
+        for(int i = 0; i < k; i++) {
+            res = Math.min(res, a[a.length - 1][i]);
+        }
+        return res;
+    }
+}
+
+/*
+class Solution {
+    public int minCostII(int[][] a) {
+        int k = a[0].length;
         for(int i = 1; i < a.length; i++) {
             for(int j = 0; j < k; j++) {
                 int min = Integer.MAX_VALUE;
@@ -20,12 +65,4 @@ class Solution {
     }
 }
 
-/*
-[[3,20,7,7,16,8,7,12,11,19,1],
-[10,14,3,3,9,13,4,12,14,13,1],
-[10,1,14,11,1,16,2,7,16,7,19],
-[13,20,17,15,3,13,8,10,7,8,9],
-[4,14,18,15,11,9,19,3,15,12,15],
-[14,12,16,19,2,12,13,3,11,10,9],
-[18,12,10,16,19,9,18,4,14,2,4]]
 */
