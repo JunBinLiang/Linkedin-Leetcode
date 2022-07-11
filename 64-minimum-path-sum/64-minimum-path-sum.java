ecff0336d519fd133f7075cb9316747f9b5c368d
66brother <45537132+JunBinLiang@users.noreply.github.com>
2022-07-10 20:52:17 -0400

    Time: 8 ms (6.64%), Space: 46 MB (49.04%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.java
+++ 64-minimum-path-sum/64-minimum-path-sum.java
@@ -5,7 +5,6 @@ class Solution {
             for(int j = 0; j < m; j++) {
                 if(i == 0 && j == 0) continue;
                 //[i, j]
-                
                 //a[i - 1][j]
                 //a[i][j - 1]
                 int min = Integer.MAX_VALUE;
@@ -17,3 +16,21 @@ class Solution {
         return a[n - 1][m - 1];
     }
 }
+
+/*
+class Solution {
+    public int minPathSum(int[][] a) {
+        int n = a.length, m = a[0].length;
+        return dfs(a, n - 1, m - 1);
+    }
+    
+    public int dfs(int a[][], int r, int c) {
+        if(r < 0 || c < 0) return 1000000000;
+        if(r == 0 && c == 0) {
+            return a[r][c];
+        }
+        return a[r][c] + Math.min(dfs(a, r - 1, c), dfs(a, r, c - 1));
+    }
+    
+}
+*/
