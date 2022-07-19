657cd0037b906cbdd7e716d1a38d7cf51f75f915
66brother <45537132+JunBinLiang@users.noreply.github.com>
2022-07-18 21:08:31 -0400

    Time: 78 ms (13.69%), Space: 72.1 MB (7.62%) - LeetHub

--- 322-coin-change/322-coin-change.java
+++ 322-coin-change/322-coin-change.java
@@ -1,14 +1,25 @@
 class Solution {
-     int dp[][];
+    
     public int coinChange(int[] coins, int amount) {
-        dp = new int[amount + 1][coins.length + 1];
-        for(int p[] : dp) Arrays.fill(p, -1);
-        int res =  dfs(coins, amount, coins.length - 1);
+        int dp[][] = new int[amount + 1][coins.length + 1];
+        for(int p[] : dp) Arrays.fill(p, 100000000);
+        dp[0][0] = 0;
+        
+        for(int i = 1; i <= coins.length; i++) {
+            int val = coins[i - 1];
+            for(int sum = 0; sum < dp.length; sum++) {
+                dp[sum][i] = Math.min(dp[sum][i], dp[sum][i - 1]);
+                if(sum - val >= 0) {
+                    dp[sum][i] = Math.min(dp[sum][i], 1 + dp[sum - val][i]);
+                }
+            }
+        }
+        int res = dp[amount][coins.length];
         if(res >= 100000000) return -1;
         return res;
     }
     
-    public int dfs(int a[], int sum, int i) {
+    /*public int dfs(int a[], int sum, int i) {
         if(i < 0) {
             if(sum == 0) return 0;
             else return 100000000;
@@ -23,5 +34,7 @@ class Solution {
             res = Math.min(res, 1 + dfs(a, sum - a[i], i)) ;
         }
         return dp[sum][i] = res;
-    }
+    }*/
+    
+    //dp[sum][i] = min(1 + dp[sum - a[i]][i], dp[sum][i - 1])
 }
