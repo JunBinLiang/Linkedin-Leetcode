8b16ad49a5d8f6eaf202178540cf584a35231afc
66brother <45537132+JunBinLiang@users.noreply.github.com>
2022-07-18 20:24:25 -0400

    Time: 107 ms (25.85%), Space: 56.8 MB (34.96%) - LeetHub

---
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.java
@@ -0,0 +1,31 @@
+class Solution {
+    Boolean dp[][];
+    public boolean canPartition(int[] a) {
+        int sum = 0;
+        for(int i : a) sum += i;
+        if(sum % 2 == 1) return false;
+        sum /= 2;
+        dp = new Boolean[sum + 1][a.length + 1];
+        return dfs(a, sum, a.length - 1);
+    }
+    
+    public boolean dfs(int a[], int sum, int i) {
+        if(i < 0) {
+            if(sum == 0) return true;
+            else return false;
+        }
+        
+        if(dp[sum][i] != null) return dp[sum][i];
+        
+        boolean res = false;
+        if(sum - a[i] >= 0) {
+            boolean x = dfs(a, sum - a[i], i - 1);
+            if(x) res = true;
+        }
+        boolean y = dfs(a, sum, i - 1);
+        if(y) res = true;
+        dp[sum][i] = res;
+        return res;
+    }
+    
+}
