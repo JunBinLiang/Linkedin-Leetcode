5f2f6177a579ddc993bafd42f36e1896c4a2d034
66brother <45537132+JunBinLiang@users.noreply.github.com>
2022-07-18 20:44:22 -0400

    Time: 101 ms (28.30%), Space: 54.5 MB (42.92%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.java
@@ -1,15 +1,28 @@
 class Solution {
-    Boolean dp[][];
+
     public boolean canPartition(int[] a) {
         int sum = 0;
         for(int i : a) sum += i;
         if(sum % 2 == 1) return false;
         sum /= 2;
-        dp = new Boolean[sum + 1][a.length + 1];
-        return dfs(a, sum, a.length - 1);
+        
+        boolean dp[][] = new boolean[sum + 1][a.length + 1];
+        dp[0][0] = true;
+        
+        for(int i = 1; i <= a.length; i++) {
+            int val = a[i - 1];
+            for(int j = 0; j < dp.length; j++) {
+                dp[j][i] |= dp[j][i - 1];
+                if(j >= val) {
+                    dp[j][i] |= dp[j - val][i - 1];
+                }
+            }
+        }
+  
+        return dp[sum][a.length];
     }
     
-    public boolean dfs(int a[], int sum, int i) {
+    /**public boolean dfs(int a[], int sum, int i) {
         if(i < 0) {
             if(sum == 0) return true;
             else return false;
@@ -26,6 +39,8 @@ class Solution {
         if(y) res = true;
         dp[sum][i] = res;
         return res;
-    }
+    }**/
+    
+    //dp[sum][i] = dp[sum - a[i]][i - 1] | dp[sum][i - 1] 
     
 }
